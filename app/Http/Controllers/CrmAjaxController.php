b7de029e834eea45930b69b4211ab14cc353aee6
Cedric VANGOUT <mail@cvng.dev>
2019-10-03 11:47:58 +0200

    Add CrmAjaxController

---
+++ app/Http/Controllers/CrmAjaxController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CrmAjaxController extends Controller
+{
+    public function handleAction(Request $request)
+    {
+    }
+}
